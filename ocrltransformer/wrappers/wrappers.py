49807df0ca19356625d6cb6daa663536c5bc09a7
vanillawhey <cedric.derstroff@gmx.net>
2025-06-06 12:50:57 +0200

    Add: polar coordinates

--- ocrltransformer/wrappers/wrappers.py
+++ ocrltransformer/wrappers/wrappers.py
@@ -8,10 +8,12 @@ from gymnasium.spaces import Sequence, Box
 from ocatari.ram.extract_ram_info import get_class_dict, get_max_objects
 from ocatari.ram.game_objects import NoObject
 
+
 class SeqSpace(Sequence):
     @property
     def shape(self):
         return self.feature_space.shape
+
 
 class OCWrapper(ObservationWrapper):
     """
@@ -26,6 +28,7 @@ class OCWrapper(ObservationWrapper):
         new_obs = np.swapaxes(observation, 0, 1)
         return np.reshape(new_obs, (3, -1))[:, 4:]
 
+
 class EgoCentricObsWrapper(ObservationWrapper):
     """
     A Wrapper
@@ -38,6 +41,7 @@ class EgoCentricObsWrapper(ObservationWrapper):
         obs = observation - observation[self.ego_pos]
         obs[self.ego_pos] = observation[self.ego_pos]
         return obs
+
 
 class ObjectLambdaWrapper(ObservationWrapper):
     def __init__(self, env, obs_lambda):
@@ -58,10 +62,17 @@ class ObjectLambdaWrapper(ObservationWrapper):
                 i += 1
         return state
 
+
 class EgoCentricWrapper(ObservationWrapper):
-    def __init__(self, env, player_name="Player", include_type=False):
+    def __init__(self, env, player_name="Player", include_type=False,
+                 use_polar_coordinates=False, relative_velocity=True):
         super().__init__(env)
         self.player_name = player_name
+        self.use_polar_coordinates = use_polar_coordinates
+        if relative_velocity:
+            self.relative_pv_index = 4
+        else:
+            self.relative_pv_index = 2
 
         max_objs = get_max_objects(env.game_name, env.hud) # noqa: type(env) == OCAtari
         self.num_obj = len(max_objs)
@@ -78,13 +89,30 @@ class EgoCentricWrapper(ObservationWrapper):
         for o in self.env.objects:  # noqa: type(env) == OCAtari
             if not (o is None or o.category == "NoObject"):
                 if player_idx == -1 and o.category == self.player_name:
-                    player_pos = o.center
+                    player_pos_v = [o.dx, o.dy, *o.center]
                     player_idx = i
                 state[i] = self.object_types[o.category]
                 state[i, -4:-2] = o.dx, o.dy
                 state[i, -2:] = o.center
                 i += 1
         if player_idx != -1:  # sometimes the player disappears on termination
-            state[:, -2:] -= player_pos  # noqa: position is always set
-            state[player_idx, -2:] = player_pos
+            # either calculate relative pos and velocity or just pos
+            state[:, -self.relative_pv_index:] -= player_pos_v[-self.relative_pv_index:]  # noqa: is always set
+            state[player_idx, -self.relative_pv_index:-2] = player_pos_v[-self.relative_pv_index:-2]
+            if self.use_polar_coordinates:
+                # position
+                state[:, -2:] = get_polar_coordinates(state[:, -2:])
+                # velocity
+                state[:, -4:-2] = get_polar_coordinates(state[:, -4:-2])
+
+            state[player_idx, -2:] = player_pos_v[-2:]
         return state
+
+
+# xys.shape = (batch, obs, 2)
+def get_polar_coordinates(xys):
+    """Returns the polar coordinates of a point."""
+    polar_coordinates = np.empty_like(xys)
+    polar_coordinates[:, 0] = np.sqrt(np.sum(xys**2, axis=1))
+    polar_coordinates[:, 1] = np.arctan2(xys[:, 0], xys[:, 1])  # angle in radians
+    return polar_coordinates
