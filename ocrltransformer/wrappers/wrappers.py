e0a72288f7bd5bf230d6388a1ea67137281fd8ab
vanillawhey <cedric.derstroff@gmx.net>
2025-10-14 21:02:18 +0200

    Add support for width and height; Add flatten wrapper for obj like architectures.

--- ocrltransformer/wrappers/wrappers.py
+++ ocrltransformer/wrappers/wrappers.py
@@ -2,6 +2,7 @@ import numpy as np
 
 import gymnasium as gym
 from gymnasium import ObservationWrapper
+from gymnasium.core import ObsType, WrapperObsType
 from gymnasium.spaces import Sequence, Box
 
 
@@ -13,20 +14,6 @@ class SeqSpace(Sequence):
     @property
     def shape(self):
         return self.feature_space.shape
-
-
-class OCWrapper(ObservationWrapper):
-    """
-    A Wrapper
-    """
-    def __init__(self, env: gym.Env):
-        self.object_types = {k: i for i, k in enumerate(get_class_dict(env.game_name).keys())}
-        self.num_objects = len(self.object_types)
-        super().__init__(env)
-
-    def observation(self, observation):
-        new_obs = np.swapaxes(observation, 0, 1)
-        return np.reshape(new_obs, (3, -1))[:, 4:]
 
 
 class EgoCentricObsWrapper(ObservationWrapper):
@@ -47,14 +34,14 @@ class ObjectLambdaWrapper(ObservationWrapper):
     def __init__(self, env, obs_lambda):
         super().__init__(env)
         self.obs_lambda = obs_lambda
-        self.feature_size = obs_lambda(NoObject()).shape
-        self.observation_space = SeqSpace(Box(-np.inf, np.inf, shape=self.feature_size),
+        self.brutto_feature_size = len(obs_lambda(NoObject()))
+        self.observation_space = SeqSpace(Box(-np.inf, np.inf, shape=self.brutto_feature_size),
                                           stack=True)
         self.max_len = sum(get_max_objects(env.game_name, env.hud).values()) # noqa: type(env) == OCAtari
 
 
     def observation(self, observation):
-        state = np.zeros((self.max_len, self.feature_size))
+        state = np.zeros((self.max_len, self.brutto_feature_size))
         i = 0
         for o in self.env.objects:  # noqa: type(env) == OCAtari
             if not (o is None or o.category == "NoObject"):
@@ -65,7 +52,8 @@ class ObjectLambdaWrapper(ObservationWrapper):
 
 class EgoCentricWrapper(ObservationWrapper):
     def __init__(self, env, player_name="Player", type_embedding=None,
-                 use_polar_coordinates=False, relative_velocity=True):
+                 use_polar_coordinates=False, relative_velocity=True,
+                 include_wh=False):
         super().__init__(env)
 
         assert type_embedding in [None, "one_hot", "additive"]
@@ -78,33 +66,42 @@ class EgoCentricWrapper(ObservationWrapper):
             self.relative_pv_index = 2
 
         max_objs = get_max_objects(env.game_name, env.hud) # noqa: type(env) == OCAtari
-        self.num_obj = len(max_objs)
+        self.num_object_types = len(max_objs)
         self.max_len = sum(max_objs.values())
-        self.feature_size = 4
+
+        if include_wh:
+            self.feature_func = w_h_dx_dy_center
+        else:
+            self.feature_func = dx_dy_center
+
+        self.netto_feature_size = len(self.feature_func(NoObject()))  # object features only
+        self.brutto_feature_size = self.netto_feature_size  # may include type embedding etc.
 
         if type_embedding == "one_hot":
-            self.feature_size += self.num_obj
-            self.object_types =  {k: np.eye(1, self.feature_size, i) for i, k in enumerate(max_objs.keys())}
+            self.brutto_feature_size += self.num_object_types
+            self.object_types =  {k: np.eye(1, self.brutto_feature_size, i) for i, k in enumerate(max_objs.keys())}
         elif type_embedding == "additive":
-            self.object_types = {k: positional_encode(self.feature_size, i) for i, k in enumerate(max_objs.keys())}
+            self.object_types = {k: positional_encode(self.brutto_feature_size, i) for i, k in enumerate(max_objs.keys())}
+        elif type_embedding is None:
+            self.object_types = {k: np.zeros((1, self.brutto_feature_size)) for k in max_objs.keys()}
         else:
-            self.object_types = {k: np.zeros((1, self.feature_size)) for k in max_objs.keys()}
+            raise AttributeError(f"Type embedding {type_embedding} not supported!")
 
-        self.observation_space = Box(-np.inf, np.inf, shape=(self.max_len, self.feature_size))
+        self.observation_space = Box(-np.inf, np.inf, shape=(self.max_len, self.brutto_feature_size))
 
     def observation(self, observation):
-        state = np.zeros((self.max_len, self.feature_size))
-        emb = np.zeros((len(self.env.objects), self.feature_size))
+        state = np.zeros((self.max_len, self.brutto_feature_size))
+        emb = np.zeros((len(self.env.objects), self.brutto_feature_size))
         i = 0
         player_idx = -1
         for o in self.env.objects:  # noqa: type(env) == OCAtari
             if not (o is None or o.category == "NoObject"):
-                if player_idx == -1 and o.category == self.player_name:
-                    player_pos_v = [o.dx, o.dy, *o.center]
+                center_x, center_y = o.center  # only claculate once
+                if o.category == self.player_name and player_idx == -1:
+                    player_pos_v = [o.dx, o.dy, center_x, center_y]
                     player_idx = i
                 emb[i] = self.object_types[o.category]
-                state[i, -4:-2] = o.dx, o.dy
-                state[i, -2:] = o.center
+                state[i, -self.netto_feature_size:] = self.feature_func(o)
                 i += 1
         if player_idx != -1:  # sometimes the player disappears on termination
             # either calculate relative pos and velocity or just pos
@@ -124,6 +121,16 @@ class EgoCentricWrapper(ObservationWrapper):
         return state
 
 
+class ObjFlatObsWrapper(ObservationWrapper):
+    def __init__(self, env):
+        super().__init__(env)
+        self.observation_space = Box(-np.inf, np.inf, shape=(env.max_len * env.brutto_feature_size,))
+
+    def observation(self, observation):
+        return observation.flatten()
+
+
+
 # xys.shape = (batch, obs, 2)
 def get_polar_coordinates(xys):
     """Returns the polar coordinates of a point."""
@@ -139,3 +146,15 @@ def positional_encode(d_model, position):
     pe[0::2] = np.sin(position * div_term)
     pe[1::2] = np.cos(position * div_term)
     return pe
+
+
+# object feature functions
+def dx_dy_center(o):
+    center_x, center_y = o.center
+    return o.dx, o.dy, center_x, center_y
+
+
+def w_h_dx_dy_center(o):
+    center_x, center_y = o.center
+    w, h = o.wh
+    return w, h, o.dx, o.dy, center_x, center_y
