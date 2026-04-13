5168837ada7042c9e3310c8b5c8a2279d70fc4e1
vanillawhey <cedric.derstroff@gmx.net>
2026-04-13 09:37:05 +0200

    Add: Zero Player (relative to itself); Add RandomCropWrapper

--- ocrltransformer/wrappers/wrappers.py
+++ ocrltransformer/wrappers/wrappers.py
@@ -1,13 +1,19 @@
+from collections import deque
+
 import numpy as np
 
 import gymnasium as gym
+import torch
 from gymnasium import ObservationWrapper
-from gymnasium.core import ObsType, WrapperObsType
 from gymnasium.spaces import Sequence, Box
-
 
 from ocatari.ram.extract_ram_info import get_class_dict, get_max_objects
 from ocatari.ram.game_objects import NoObject
+from torchvision.transforms import RandomCrop, functional as F
+
+from copy import copy
+
+import cv2
 
 
 class SeqSpace(Sequence):
@@ -53,7 +59,7 @@ class ObjectLambdaWrapper(ObservationWrapper):
 class EgoCentricWrapper(ObservationWrapper):
     def __init__(self, env, player_name="Player", type_embedding=None,
                  use_polar_coordinates=False, relative_velocity=True,
-                 include_wh=False):
+                 include_wh=False, zero_player=False):
         super().__init__(env)
 
         self.player_name = player_name
@@ -87,6 +93,8 @@ class EgoCentricWrapper(ObservationWrapper):
 
         self.observation_space = Box(-np.inf, np.inf, shape=(self.max_len, self.brutto_feature_size))
 
+        self.zero_player = zero_player
+
     def observation(self, observation):
         state = np.zeros((self.max_len, self.brutto_feature_size))
         emb = np.zeros((len(self.env.objects), self.brutto_feature_size))
@@ -94,7 +102,7 @@ class EgoCentricWrapper(ObservationWrapper):
         player_idx = -1
         for o in self.env.objects:  # noqa: type(env) == OCAtari
             if not (o is None or "NoObject" in o.category):
-                center_x, center_y = o.center  # only claculate once
+                center_x, center_y = o.center  # only calculate once
                 if o.category == self.player_name and player_idx == -1:
                     player_pos_v = [o.dx, o.dy, center_x, center_y]
                     player_idx = i
@@ -104,14 +112,17 @@ class EgoCentricWrapper(ObservationWrapper):
         if player_idx != -1:  # sometimes the player disappears on termination
             # either calculate relative pos and velocity or just pos
             state[:i, -self.relative_pv_index:] -= player_pos_v[-self.relative_pv_index:]  # noqa: is always set
-            state[player_idx, -self.relative_pv_index:-2] = player_pos_v[-self.relative_pv_index:-2]
+
+            if not self.zero_player:  # otherwise keep player values as 0
+                state[player_idx, -self.relative_pv_index:-2] = player_pos_v[-self.relative_pv_index:-2]
             if self.use_polar_coordinates:
                 # position
                 state[:i, -2:] = get_polar_coordinates(state[:i, -2:])
                 # velocity
                 state[:i, -4:-2] = get_polar_coordinates(state[:i, -4:-2])
 
-            state[player_idx, -2:] = player_pos_v[-2:]
+            if not self.zero_player:  # otherwise keep player values as 0
+                state[player_idx, -2:] = player_pos_v[-2:]
 
         # object type
         state[:i] += emb[:i]
@@ -127,6 +138,66 @@ class ObjFlatObsWrapper(ObservationWrapper):
     def observation(self, observation):
         return observation.flatten()
 
+
+class RandomCropWrapper(RandomCrop, ObservationWrapper):
+    def __init__(self, env, padding, obs_mode="ori"):
+        super().__init__(env.observation_space.shape[:2], padding, padding_mode="edge")
+        super(ObservationWrapper, self).__init__(env)
+
+        self.objects = []
+        self.offset_x, self.offset_y = 0, 0
+        self.h, self.w = 0, 0
+
+        self._state_buffer_dqn = deque([], 4)
+
+        if type(padding) == int:
+            self.pads = (padding, padding)
+        else:
+            self.pads = padding[:2]
+
+        if obs_mode == "ori":
+            self.obs = lambda o: o
+        elif obs_mode == "dqn":
+            self.obs = self.down_scale
+        else:
+            raise NotImplementedError(f"obs_mode {obs_mode} is not supported!")
+
+    def down_scale(self, obs):
+        dqn_obs = cv2.resize(cv2.cvtColor(obs, cv2.COLOR_RGB2GRAY), (84, 84), interpolation=cv2.INTER_AREA)
+        self._state_buffer_dqn.append(dqn_obs)
+
+        return np.array(self._state_buffer_dqn)
+
+    def get_params(self, img, output_size):
+        return self.offset_y, self.offset_x, self.h, self.w
+
+    def observation(self, observation):
+        new_obs = self(torch.tensor(observation).moveaxis(-1, 0))
+
+        self.objects = []
+        for obj in self.env.objects:
+            if not (obj is None or "NoObject" in obj.category):
+                o = copy(obj)
+                o.xy = (obj.x - self.offset_x + self.pads[0], obj.y - self.offset_y + self.pads[1])
+                self.objects.append(o)
+
+        return self.obs(new_obs.moveaxis(0, -1).numpy())
+
+    def reset(self, **kwargs):
+        obs, info = self.env.reset(**kwargs)
+
+        img = F.pad(torch.tensor(obs).moveaxis(-1, 0), self.padding, self.fill, self.padding_mode)
+        self.offset_y, self.offset_x, self.h, self.w = super().get_params(img, self.size)
+
+        # fill buffer
+        self.observation(obs)
+        self.observation(obs)
+        self.observation(obs)
+
+        return self.observation(obs), info
+
+    def __getattr__(self, name: str):
+        return super(ObservationWrapper, self).__getattr__(name)
 
 
 # xys.shape = (batch, obs, 2)
